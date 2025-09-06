2a6a2d42cf569dacc5dc425644474e0b7e91cd16
Minakshi Das <104790941+minakshi240@users.noreply.github.com>
2025-09-06 23:11:46 +0530

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const fullDarkModeCheckbox = document.querySelector('#full-dark-mode')
+const containedDarkModeCheckbox = document.querySelector('#contained-dark-mode')
+const container = document.querySelector('.container')
+
 
 fullDarkModeCheckbox.addEventListener('change', (e) => {
   if(fullDarkModeCheckedbox.checked) {
     document.body.classList.add('dark')
   } else {
     document.body.classList.remove('dark')
+  }
+})
+
+
+ containedDarkModeCheckbox.addEventListener('change', () => {
+  if(containedDarkModeCheckbox.checked) {
+    container.classList.add('dark')
+  } else {
+    container.classList.remove('dark')
   }
 })
